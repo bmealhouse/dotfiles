bb9aff7cf5892400dce26cf259ba80322ee54c7d
Brent Mealhouse <bmealhouse@gmail.com>
2023-12-17 20:29:20 -0600

    Add stylua config

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,37 +1,37 @@
 local plugins = {
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			auto_install = true,
-			ensure_installed = {
-				"lua",
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        "lua",
+      },
+    },
+  },
 
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			automatic_installation = true,
-			ensure_installed = {
-				"lua-language-server",
-				"stylua",
-			},
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      automatic_installation = true,
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+      },
+    },
+  },
 
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		init = function()
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-		config = function()
-			require("custom.configs.formatter")
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+    config = function()
+      require "custom.configs.formatter"
+    end,
+  },
 }
 
 return plugins
